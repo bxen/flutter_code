ebf8aec903f83fa8b8553b9a1c2db41fa52b3218
benyapa <bkaranut@gmail.com>
2025-09-04 16:35:38 +0700

    Update app to get input by button

--- lib/week04/input_demo.dart
+++ lib/week04/input_demo.dart
@@ -9,25 +9,26 @@ class InputDemo extends StatefulWidget {
 
 class _InputDemoState extends State<InputDemo> {
   String message = "";
+  TextEditingController tcName =TextEditingController();
   
 
-  void updateMessage(String str){
-    setState(() {
-      message = str;
-    });
+  void updateMessage() {
+   setState(() {
+     message = tcName.text;
+   });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Input Demo")),
       body: Column(
         children: [
-          TextField(
-            onChanged: (String str) => updateMessage(str),
-          ),
-          Text(message)
+          TextField(controller: tcName,),
+          ElevatedButton(onPressed: updateMessage, child: Text('OK')),
+          Text(message),
         ],
-      )
+      ),
     );
   }
 }
