556266a1c73c2cdd3c94c9cb71e08e30a70111ce
benyapa <bkaranut@gmail.com>
2025-09-04 16:24:55 +0700

    Create an app input demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter_code/week01/basic.dart';
-import 'package:flutter_code/week04/counter.dart';
+// import 'package:flutter_code/week04/counter.dart';
+import 'package:flutter_code/week04/input_demo.dart';
 //  import './week01/basic.dart'; in short
 
 void main(){
   runApp(MaterialApp(home:
-   Counter()
+   InputDemo()
   ));
 }
